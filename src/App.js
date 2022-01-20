43a9e8bae733a247cf823c6cd614b68c28da20f4
rriver2 <rriver2@naver.com>
2022-01-20 10:54:23 +0900

    modify ManagePage's func

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import AccessPage from './AccessPage';
 import ManagementPage from './ManagementPage';
 import MainPage from './MainPage';
+import { Button } from 'bootstrap';
 
 
 function App() {
@@ -13,12 +14,71 @@ function App() {
     <div>
       {/* <AccessPage></AccessPage> */}
 
-      {/* <ManagementPage></ManagementPage> */}
+      <ManagementPage></ManagementPage>
 
-      <MainPage></MainPage>
+      {/* <MainPage></MainPage> */}
+
+      {/* <Hi></Hi> */}
 
     </div>
   );
+}
+
+
+function Hi(){
+  const [checkedWaiting, setCheckedWaiting] = useState([]);
+
+  const changeHandler = (checked, id) => {
+    if (checked) {
+      setCheckedWaiting([...checkedWaiting, id]);
+    } else {
+      // 체크 해제
+      setCheckedWaiting(checkedWaiting.filter((el) => el !== id));
+    }
+    console.log(checked)
+  };
+
+  let [waiting,setWaiting] = useState([{  
+    "studentID" : "1111111111",
+     "name" : "대기1"
+    } ,
+  { 
+     "studentID" : "222222222",
+     "name" : "대기2"
+  },
+  {
+     "studentID" : "333333333",
+     "name" : "대기3"
+  }]);
+  
+  return (
+    <div>
+      {
+        waiting.map((waitingStudent)=>{
+          return(
+            <div>
+              <input 
+                id= {waitingStudent}
+                type="checkbox" 
+                onChange={(e)=>{
+                  changeHandler(e.currentTarget.checked, waitingStudent)
+                }}
+                checked={checkedWaiting.includes(waitingStudent) ? true : false}
+              />
+            </div>
+          )
+        })
+        }
+    <button className='searchButton' onClick={()=>{
+        checkedWaiting.map((checkedWaiting)=>{
+          console.log(checkedWaiting)
+        })
+    }}>button</button>
+                   
+    </div>
+                   
+  	
+  )
 }
 
 export default App;
--- src/ManagementPage.js
+++ src/ManagementPage.js
@@ -4,20 +4,23 @@ import './ManagementPage.css';
 
 function ManagementPage(){
 
-    let [waiting,setWaiting] = useState([{  
-        "studentID" : "1111111111",
-         "name" : "대기1"
+    const [waiting,setWaiting] = useState([
+        {  
+            "studentID" : "1111111111",
+            "name" : "대기1"
         } ,
-      { 
-         "studentID" : "222222222",
-         "name" : "대기2"
-      },
-      {
-         "studentID" : "333333333",
-         "name" : "대기3"
-      }]);
-    let [refusal,setRefusal] = useState({});
-    let [approval,setApproval] = useState([{  
+        { 
+            "studentID" : "222222222",
+            "name" : "대기2"
+        },
+        {
+            "studentID" : "333333333",
+            "name" : "대기3"
+        }
+    ]);
+
+    const [refusal,setRefusal] = useState([]);
+    const [approval,setApproval] = useState([{  
         "studentID" : "111111111",
          "name" : "승인1"
       } ,
@@ -38,30 +41,118 @@ function ManagementPage(){
         "name" : "승인5"
      }]);
 
+     const [leftTable, setLeftTable] = useState([...waiting]);
+     const [rightTable, setRightTable] = useState([...approval]);
+
+     const [leftCheckedList, setLeftCheckedList] = useState([]);
+     const [rightCheckedList, setRightCheckedList] = useState([]);
+
+     const changeHandler = (checked, id, setPlace, place) => {
+       if (checked) {
+        setPlace([...place, id]);
+       } else {
+         // 체크 해제
+         setPlace(place.filter((e) => e !== id));
+       }
+       console.log(checked)
+     };
+
+    const [searchStudent, setSearchStudent] = useState("");
+    const [searchKinds,setSearchKinds] = useState("");
+    const [searchstudentList, setSearchStudentList] = useState([]);
+    const [searchButton, setSearchButton] = useState("search");
+
     return(
+        
         <div className="ManagementPageContainer">
             <Navbar expand="lg" style={{padding: "30px 0"}}>
                 <Container fluid style={{justifyContent: "center", backgroundColor: "none"}}>
                     <h2>학생승인 현황</h2>
                     <Form  className="d-flex" >
                     <NavDropdown id="nav-dropdown" >
-                        <NavDropdown.Item eventKey="4.1">이름</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="4.2">학번</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="name" onClick={(e)=>{
+                            setSearchKinds("이름");
+                        }}>이름</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="number" onClick={(e)=>{
+                            setSearchKinds("학번");
+                        }}>학번</NavDropdown.Item>
                     </NavDropdown>
-                        <FormControl style={{border: "none"}}
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                        />
-                        <button className='searchButton'><i className="fas fa-search"></i></button>
+                        <input className="searchBar" onChange={(e) => { setSearchStudent(e.target.value); setSearchButton("search");}} 
+                            name="q" value={searchStudent} type="search" placeholder="Search" ></input>
+                            
+                        <button onClick={(e) => {
+                            if( searchButton === "x"){
+                                setSearchStudent("");
+                                setSearchButton("search");
+                                console.log(leftTable);
+                                setLeftTable([...waiting]);
+                                setRightTable([...approval]);
+                            }else{
+                                if(searchKinds === "" || searchStudent=== ""){
+                                    alert("검색 카테고리를 정하고, 검색명을 입력해주세요 :)");
+                                }else if(searchKinds === "이름"){
+                                    setSearchButton("x");
+
+                                    console.log( waiting);
+                            
+                                    let left = waiting.filter((item) => item.name === searchStudent);
+                                    let right = approval.filter((item) => item.name === searchStudent);
+        
+                                    console.log(left);
+                                    setLeftTable(left);
+                                    setRightTable(right);
+                                }else if(searchKinds === "학번"){
+                                    // setSearchButton("x");
+                                    // console.log("학번 찾기")
+                            
+                                    // let left = waiting.filter((item) => item.stID === String(searchStudent));
+                                    // let right = approval.filter((item) => item.stID === String(searchStudent));
+        
+                                    // console.log(left);
+                                    // setLeftTable(left);
+                                    // setRightTable(right);
+                                }
+                                
+                            }
+                        }}
+                            // var ob = [{
+                            //     name: 'john',
+                            //     surname: 'fox'
+                            //   }
+                            // ];
+                            // var searchText = 'fox';
+                            // var res = ob.filter((item)=>{
+                            //   return Object.keys(item).some((key)=>item[key].includes(searchText));
+                            // });
+                            // console.log(res);
+                        
+                            className='searchButton' type='button'>
+                                {
+                                searchButton === "search"
+                                ?<i className="fas fa-search"></i>
+                                :<i className="fas fa-times"></i>
+                                }
+                                </button>
                     </Form>
                 </Container>
             </Navbar>
             <div className='tables'>
                 <div className='tableSet'>
                     <div className= "buttons">
-                     <button className='submitButton'> 승인</button> <button className='submitButton'>거절</button>
+                        <button className='submitButton' onClick={()=>{
+                                console.log("승인")
+                                leftCheckedList.map((leftCheckedList)=>{
+                                console.log(leftCheckedList)
+                                })
+                                setLeftCheckedList([]);
+                            }}>승인</button> 
+                        <button className='submitButton' onClick={()=>{
+                                console.log("거절")
+                                leftCheckedList.map((leftCheckedList)=>{
+                                console.log(leftCheckedList)
+                                })
+                                setLeftCheckedList([]);
+                            }}>거절</button>
                     </div>
                     <table>
                         <thead>
@@ -71,12 +162,19 @@ function ManagementPage(){
                         </thead>
                         <tbody>
                             {
-                                waiting.map((student,i)=>{
+                                leftTable.map((student,i)=>{
                                     return(
                                         <tr key={i}>
                                             <td>{student.studentID}</td>
                                             <td>{student.name}</td>
-                                            <td><Form.Check aria-label="option 1" id = "checkBox" /></td>
+                                            <td><input 
+                                                    id= {student}
+                                                    type="checkbox" 
+                                                    onChange={(e)=>{
+                                                    changeHandler(e.currentTarget.checked, student ,setLeftCheckedList,leftCheckedList)
+                                                    }}
+                                                    checked={leftCheckedList.includes(student) ? true : false}
+                                            /></td>
                                         </tr>
                                     )
                                 })
@@ -96,10 +194,27 @@ function ManagementPage(){
 
                 <div className='tableSet'>
                     <div className= "buttons">
-                        <button className='submitButton' style={{width : "110px"}}>회장권한위임</button> <button className='submitButton'>대기</button>
+                        <button className='submitButton' style={{width : "110px"}} onClick={()=>{
+                                console.log("회장권한위임")
+                                rightCheckedList.map((rightCheckedList)=>{
+                                console.log(rightCheckedList)
+                                })
+                                if(rightCheckedList.length === 1){
+                                    setRightCheckedList([]);
+                                }else{
+                                    alert("학생회장 위임은 한명만 가능합니다.");
+                                }
+                            }}>회장권한위임</button> 
+                        <button className='submitButton' onClick={()=>{
+                                console.log("대기")
+                                rightCheckedList.map((rightCheckedList)=>{
+                                console.log(rightCheckedList)
+                                })
+                                setRightCheckedList([]);
+                            }}
+                        >대기</button>
                     </div>
                      <table>
-                        <div></div>
                         <thead>
                             <tr>
                                 <th colSpan={"3"}>승인완료</th>
@@ -107,12 +222,19 @@ function ManagementPage(){
                         </thead>
                         <tbody>
                         {
-                                approval.map((student,i)=>{
+                                rightTable.map((student,i)=>{
                                     return(
                                         <tr key={i}>
                                             <td>{student.studentID}</td>
                                             <td>{student.name}</td>
-                                            <td><Form.Check aria-label="option 1" id = "checkBox"/></td>
+                                            <td><input 
+                                                    id= {student}
+                                                    type="checkbox" 
+                                                    onChange={(e)=>{
+                                                    changeHandler(e.currentTarget.checked, student,setRightCheckedList,rightCheckedList)
+                                                    }}
+                                                    checked={rightCheckedList.includes(student) ? true : false}
+                                            /></td>
                                         </tr>
                                     )
                                 })
